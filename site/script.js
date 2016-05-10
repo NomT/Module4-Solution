8e85914e02e7169e6505bd656a9afd3d6980d036
NomT <tareens@gmail.com>
2016-05-10 21:45:31 +0100

    Updated

--- site/script.js
+++ site/script.js
@@ -28,55 +28,21 @@ assignment.
 WARNING!!! WARNING!!!
 
 */
-
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-//(function () {
+(function () {
 
-//var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-//console.log(" " + names);
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
- //for (var i = 0; i < names.length; i++) {
- //  var firstLetter = names[i].charAt(0).toLowerCase();
- //  console.log(" " + firstLetter)
- //  console.log("123 " + names[i]);
- //}
-
- var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  
-// hutconsole.log(charAt(3));
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-
-//names[i] = names[i].charAt(0).toLowerCase() + names[i].substr(1,names[i].length-1);
-
 for (var i = 0; i < names.length; i++) {
-   names[i] = names[i].charAt(0).toLowerCase() + names[i].substr(1,names[i].length-1);
-   var firstLetter = names[i].charAt(0).toLowerCase();
-   { 
-      if (firstLetter === 'j') {
-        console.log("GoodBye Loop: " + names[i]);
-        byeSpeaker.speakWord(names[i]); 
-        //console.log(+byeSpeaker.speakWord(names[i])); 
-        console.log("NOW Name: " +byeSpeaker.speakWord(names[i]));
-        //byeSpeaker.speakWord();
-        //console.log(byeSpeaker.speakWord() +names[i]);
-         } else  {
-        console.log("Hello Loop: " +names[i]);
-        helloSpeaker.speakWord(names[i]);   
-        //helloSpeaker.speakWord();
-        //console.log(helloSpeaker.speakWord.names([i])); 
-        //console.log(helloSpeaker.speakWord(names[i]));        
-   }
- };
-};
-  
- 
+
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -84,21 +50,18 @@ for (var i = 0; i < names.length; i++) {
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
-
-
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 12: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    helloSpeaker.speak(names[i]);
+  }
+}
 
-//   console.log("NOW Name: " +byeSpeaker.speakWord(names[i]));
-  
-// } 
-// else 
-// {
-// console.log("Bad");
-// helloSpeaker.speakWord(names[i]);
- 
-//};
+})();
